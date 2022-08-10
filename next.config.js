2dcd6484acdfbe9638e19c86a55caf8138309be7
oceanbao <oceanbao@outlook.com>
2022-08-10 16:07:22 +0800

    update: finished cv english

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+// const path = require('path');
+// const CopyPlugin = require('copy-webpack-plugin');
+
 // @ts-check
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -8,5 +11,18 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: false,
+  // webpack: (config, options) => {
+  //   config.plugins.push(
+  //     new CopyPlugin({
+  //       patterns: [
+  //         {
+  //           from: path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'),
+  //           to: 'cmaps/'
+  //         },
+  //       ],
+  //     })
+  //   )
+  //   return config
+  // },
 })
 module.exports = nextConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
-import dynamic from 'next/dynamic'
+import SinglePage from '@components/SinglePage'
 
-const DynViewer = dynamic(() => import('@components/demo'), {
-  ssr: false,
-})
+const PDF = '/oceancv_eng.pdf'
 
-export default DynViewer
+import { useEffect, useState } from 'react'
+
+const Loading = () => {
+  return (
+    <div className="absolute top-[5%] left-[50%] translate-x-[-50%]">
+      {Array.from(Array(5).keys()).map((item, i) => (
+        <div key={i} className="w-[80vw] h-20 border-2 rounded-md mx-auto">
+          <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+            <div className="w-[18vw] bg-gray-300 h-12 rounded-full "></div>
+            <div className="flex flex-col space-y-3">
+              <div className="w-[58vw] bg-gray-300 h-6 rounded-md "></div>
+              <div className="w-[58vw] bg-gray-300 h-6 rounded-md "></div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const Viewer = () => {
+  const [width, setWidth] = useState<number>(0)
+
+  useEffect(() => {
+    const scale = 0.98
+    setWidth(window.innerWidth * scale)
+  }, [])
+
+  return (
+    <div className="h-screen w-screen overflow-auto">
+      <SinglePage pdf={PDF} width={width} />
+    </div>
+  )
+}
+
+export default Viewer
