cf09855f9709ce7ad1c735089834b02890e4ed77
oceanbao <oceanbao@outlook.com>
2022-08-20 16:11:52 +0800

    add: mutating color

--- src/components/landing/typing/Typing.tsx
+++ src/components/landing/typing/Typing.tsx
@@ -1,35 +1,54 @@
 import s from './Typing.module.css'
 import cn from 'clsx'
 import ReactTypingEffect from 'react-typing-effect'
+import { useRef, useEffect } from 'react'
+
+import { randHex } from '@components/loader/Loader'
 
 const Typing = () => {
+  const boxRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const boxRefLocal = boxRef.current!
+
+    function changeColor() {
+      boxRefLocal.setAttribute('style', `--color-one: ${randHex()}; --color-two: ${randHex()}`)
+    }
+
+    const loop = setInterval(changeColor, 500)
+
+    return () => clearInterval(loop)
+  }, [])
+
   return (
-    <ReactTypingEffect
-      className={cn(s.typing)}
-      staticText="I"
-      text={['create web apps.', 'write language content.', 'visualise data.']}
-      speed={50}
-      eraseSpeed={100}
-      eraseDelay={3000}
-      typingDelay={1000}
-      // cursorRenderer={cursor => <h1>{cursor}</h1>}
-      displayTextRenderer={(text, i) => {
-        return (
-          <h1>
-            {text.split(' ').map((ss, i) => {
-              const key = `${i}`
-              let color = '#F038FF'
-              if (i === 0) color = '#FF4136'
-              return (
-                <span key={key} style={{ color }}>
-                  {ss + ' '}
-                </span>
-              )
-            })}
-          </h1>
-        )
-      }}
-    />
+    <div ref={boxRef}>
+      <ReactTypingEffect
+        className={cn(s.typing)}
+        staticText="I"
+        text={['create web apps.', 'write language content.', 'visualise data.']}
+        speed={50}
+        eraseSpeed={100}
+        eraseDelay={3000}
+        typingDelay={1000}
+        // cursorRenderer={cursor => <h1>{cursor}</h1>}
+        displayTextRenderer={(text, i) => {
+          return (
+            <h1>
+              {text.split(' ').map((ss, i) => {
+                const key = `${i}`
+                let color = 'var(--color-one)'
+                if (i === 0) color = 'var(--color-two)'
+                return (
+                  <span key={key} style={{ color }}>
+                    {ss + ' '}
+                  </span>
+                )
+              })}
+            </h1>
+          )
+        }}
+      />
+    </div>
   )
 }
 
--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,9 +1,44 @@
 import cn from 'clsx'
 import s from './Loader.module.css'
+import { useRef, useEffect } from 'react'
+
+const randomColor = () => {
+  return '#' + (((1 << 24) * Math.random()) | 0).toString(16)
+}
+
+function ChanelRand() {
+  return Math.floor(Math.random() * (256 + 1))
+}
+function rgbRand() {
+  return [ChanelRand(), ChanelRand(), ChanelRand()]
+}
+function rgbToHex(rgb: number[]) {
+  return ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1)
+}
+function colorRand() {
+  return rgbToHex(rgbRand())
+}
+
+export const randHex = () => '#' + colorRand()
+const randRGB = () => 'rgb(' + ChanelRand() + ',' + ChanelRand() + ',' + ChanelRand() + ')'
 
 const Loader = () => {
+  const boxRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const boxRefLocal = boxRef.current!
+
+    function changeColor() {
+      boxRefLocal.setAttribute('style', `--color-square-start: ${randHex()}; --color-square-end: ${randHex()}`)
+    }
+
+    const loop = setInterval(changeColor, 1500)
+
+    return () => clearInterval(loop)
+  }, [])
+
   return (
-    <div className={cn(s.loading)}>
+    <div ref={boxRef} className={cn(s.loading)}>
       <div className={cn(s.loadingCenter)}>
         <div className={cn(s.loadingAbsolute)}>
           <div className={cn(s.object, s.objectOne)}></div>
